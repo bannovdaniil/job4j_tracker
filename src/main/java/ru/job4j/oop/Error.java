f7754091cf228720bc05ed946773d4d579836e54
bannovdaniil <baddan@mail.ru>
2021-12-22 23:22:44 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public void printError() {
+        System.out.println("Active: " + this.active + " Status: "
+                + this.status + " Message: " + this.message);
+    }
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 1, "Out of memory");
+        Error error3 = new Error(false, 2, "Invalid client");
+        error1.printError();
+        error2.printError();
+        error3.printError();
+    }
+}
