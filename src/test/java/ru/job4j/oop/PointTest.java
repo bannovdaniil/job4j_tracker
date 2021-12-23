9dac46f8a6a7a7ff0418e9251f11c221d00d6158
Daniil Bannov #3 <baddan@mail.ru>
2021-12-23 11:34:06 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587 #243129]

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        assertThat(expected, closeTo(dist, 0.01));
+    }
+
+    @Test
+    public void when78to44then5() {
+        double expected = 5;
+        Point a = new Point(7, 8);
+        Point b = new Point(4, 4);
+        double dist = a.distance(b);
+        assertThat(expected, closeTo(dist, 0.01));
+    }
+
+    @Test
+    public void when10and17to2and0then18dot78() {
+        double expected = 18.78;
+        Point a = new Point(10, 17);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        assertThat(expected, closeTo(dist, 0.01));
+    }
+}
