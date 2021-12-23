303d8db6f4853ac27f410593a1f5801325094820
bannovdaniil <baddan@mail.ru>
2021-12-23 20:05:28 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
+    private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
 
@@ -30,5 +33,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
