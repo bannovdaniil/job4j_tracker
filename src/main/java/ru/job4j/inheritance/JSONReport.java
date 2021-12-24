7070a0a27b20ff0be27dc2f4d183ac2f03fb0346
bannovdaniil <baddan@mail.ru>
2021-12-24 08:04:34 +0300

    5. Аннотация @Override [#174891 #243410]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,10 +1,6 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
     @Override
     public String generate(String name, String body) {
         String ln = System.lineSeparator();
