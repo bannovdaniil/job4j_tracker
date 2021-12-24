4c545370af25a918048965999b909f8166f59791
Daniil Bannov #3 <baddan@mail.ru>
2021-12-24 11:45:09 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,21 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] temp = new Item[100];
+        int count = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (key.equals(items[i].getName())) {
+                temp[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(temp, count);
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(this.items, this.size);
     }
 
     public Item findById(int id) {
