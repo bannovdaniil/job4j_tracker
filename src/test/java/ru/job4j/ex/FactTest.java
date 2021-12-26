42f80c5f693460e88bde87a17aa0ea5c6fcb69ea
bannovdaniil <baddan@mail.ru>
2021-12-26 08:02:11 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCalc0ThenExeption() {
+        new Fact().calc(-1);
+    }
+
+    @Test
+    public void whenCalc5Then120() {
+        int in = 5;
+        int expected = 120;
+        Fact fact = new Fact();
+        int result = fact.calc(in);
+        assertThat(expected, is(result));
+    }
+}
