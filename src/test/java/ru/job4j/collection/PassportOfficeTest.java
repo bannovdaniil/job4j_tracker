df9fa0d70d801ca5291c9502e7830586be3aa2a4
bannovdaniil <baddan@mail.ru>
2021-12-28 15:06:49 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void whenOneAdd() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenTwoAdd() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("3f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        office.add(citizen2);
+        assertThat(office.get(citizen1.getPassport()), is(citizen1));
+        assertThat(office.get(citizen2.getPassport()), is(citizen2));
+    }
+
+    @Test
+    public void whenEqualsTwoAddThenFalse() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        boolean expected = office.add(citizen2);
+        assertThat(office.get(citizen1.getPassport()), is(citizen1));
+        assertThat(expected, is(false));
+    }
+}
