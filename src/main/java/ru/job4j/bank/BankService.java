f20acc1208dbb2134f8c25d96ff99ac33b59a31f
bannovdaniil <baddan@mail.ru>
2021-12-28 15:58:49 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,62 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+                users.put(user, accounts);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (passport.equals(user.getPassport())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (accounts != null) {
+                for (Account account : accounts) {
+                    if (requisite.equals(account.getRequisite())) {
+                        return account;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account account1 = findByRequisite(srcPassport, srcRequisite);
+        if (account1.getBalance() >= amount) {
+            account1.setBalance(account1.getBalance() - amount);
+            Account account2 = findByRequisite(destPassport, destRequisite);
+            account2.setBalance(account2.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
