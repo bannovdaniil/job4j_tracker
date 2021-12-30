ed76fa9e8e3eaddae70cf2146470362fa5c11a5b
bannovdaniil <baddan@mail.ru>
2021-12-30 19:27:55 +0300

    0. Stream API [#58166]

---
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 1. создается рандомный список от -4 до 5
+ * 2. показываем то что получилось на консоль
+ * 3. оставляем только <0
+ * 4. показываем то что осталось
+ * toList() vs collect(Collectors.toList()) появилось только в 16+ версии Java
+ */
+public class FilterNegativeNumbers {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Stream.generate(
+                () -> 6 - new Random().nextInt(10)).limit(10).toList();
+        System.out.println("In:");
+        numbers.forEach((n) -> System.out.print(n + ", "));
+        System.out.println("\nOut:");
+        List<Integer> positive = numbers.stream().filter(n -> n < 0).collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
