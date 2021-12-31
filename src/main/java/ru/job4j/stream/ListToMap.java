837b603a04d5dc8731630ccca0293693069ff363
bannovdaniil <baddan@mail.ru>
2021-12-31 09:06:46 +0300

    4. Преобразование List в Map. [#110226 #245555]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -14,14 +14,17 @@ public class ListToMap {
      * про heshSet, но если сразу добавить, то неувидим не одного дубля
      * а нужен хотябы одни, составную lambda приделать не удалось
      * и суппер add возращает, добавился элемент или нет!
+     * <-- Обнулились
+     * в toMap приходит 3 параметра
+     *
      * @param list - Студенты
      * @return - коллекция студентов
      */
     public static Map<String, Student> convert(List<Student> list) {
-        Set<String> uniq = new HashSet<>();
         return list.stream()
-                .filter(x -> uniq.add(x.getSurname()))
                 .collect(Collectors
-                        .toConcurrentMap(student -> student.getSurname(), student -> student));
+                        .toConcurrentMap(
+                                st -> st.getSurname(), st -> st,
+                                (st1, st2) -> st1));
     }
 }
