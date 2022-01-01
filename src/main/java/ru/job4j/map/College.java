39d36c6138b4420351a6c80e25173a372f65a61e
bannovdaniil <baddan@mail.ru>
2022-01-01 11:28:44 +0300

    3. Optional в банковских переводах. [#214711 #245730]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -12,14 +12,10 @@ public class College {
     }
 
     public Optional<Student> findByAccount(String account) {
-        Optional<Student> rsl = Optional.empty();
-        for (Student s : students.keySet()) {
-            if (account.equals(s.getAccount())) {
-                rsl = Optional.of(s);
-                break;
-            }
-        }
-        return rsl;
+        return students.keySet()
+                .stream()
+                .filter(s -> account.equals(s.getAccount()))
+                .findFirst();
     }
 
     /**
@@ -27,8 +23,9 @@ public class College {
      * с null поинтером, чтобы извлечь не Null, к переменной желательно
      * добавить еще и .get() но ошибка не явная, в тестах без этого валится
      * интерпритатор молчит, поэтому внимательность и еще раз внимательность...
+     *
      * @param account - номер студента в списке ?
-     * @param name - имя студента
+     * @param name    - имя студента
      * @return - предметы
      */
     public Optional<Subject> findBySubjectName(String account, String name) {
@@ -36,12 +33,11 @@ public class College {
         Optional<Student> s = findByAccount(account);
         if (s.isPresent()) {
             Set<Subject> subjects = students.get(s.get());
-            for (Subject subj : subjects) {
-                if (name.equals(subj.getName())) {
-                    rsl = Optional.of(subj);
-                    break;
-                }
-            }
+            rsl = subjects
+                    .stream()
+                    .filter(subj -> Optional.ofNullable(subj).isPresent())
+                    .filter(subj -> name.equals(subj.getName()))
+                    .findFirst();
         }
         return rsl;
     }
