c418254c716e94e88949854bf4b59a93c92c37d6
bannovdaniil <baddan@mail.ru>
2022-01-22 09:14:31 +0300

    3. Liquibase. Интеграционные тесты. [#95881 #253952]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import ru.job4j.tracker.Item;
 
 import java.io.InputStream;
 import java.sql.Connection;
